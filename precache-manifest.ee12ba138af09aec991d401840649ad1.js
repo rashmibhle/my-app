87c1e26614174acabd127d9ed9663964b1f54061
rashmibhle <rashmibhle@gmail.com>
2019-12-01 22:58:40 +0530

    Updates

---
+++ precache-manifest.ee12ba138af09aec991d401840649ad1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cd58211864e20f23bf16bcd22bb5b60",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "1143c5b3603c80bf4651",
+    "url": "/my-app/static/css/main.eb794fda.chunk.css"
+  },
+  {
+    "revision": "b97124775186cd052c84",
+    "url": "/my-app/static/js/2.64a083c5.chunk.js"
+  },
+  {
+    "revision": "1143c5b3603c80bf4651",
+    "url": "/my-app/static/js/main.4eea13bc.chunk.js"
+  },
+  {
+    "revision": "0e179b6188a25da714f2",
+    "url": "/my-app/static/js/runtime-main.07079bbf.js"
+  }
+]);
